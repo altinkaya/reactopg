4ecc5a734e9a7e4e66a0370e6912048d7ff4435e
altinkaya <124771353+altinkaya@users.noreply.github.com>
2024-05-06 11:41:15 +0200

    zc
    zxc

---
+++ src/main/java/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+import ReST.ApplicationConfig;
+import Routes.Route;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationConfig app = ApplicationConfig.getInstance();
+        app.initiateServer()
+                .startServer(7000)
+                .setExceptionHandlers()
+                .checkSecurityRoles()
+                .setRoute(Route.addRoutes());
+    }
+}
