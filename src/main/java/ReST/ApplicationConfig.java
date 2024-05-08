a5287e41c013753c801c8cbda51422da262c2f9b
altinkaya <124771353+altinkaya@users.noreply.github.com>
2024-05-09 00:40:07 +0200

    earf
    sdf

--- src/main/java/ReST/ApplicationConfig.java
+++ src/main/java/ReST/ApplicationConfig.java
@@ -110,13 +110,13 @@ public class ApplicationConfig {
 
     public ApplicationConfig configureCors() {
         app.before(ctx -> {
-            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Origin", "http://localhost:5173/");
             ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             ctx.header("Access-Control-Allow-Headers", "Content-Type");
         });
 
         app.options("/*", ctx -> {
-            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Origin", "http://localhost:5173/");
             ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             ctx.header("Access-Control-Allow-Headers", "Content-Type");
         });
